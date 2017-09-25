02006d424ddd8dc2585c98d162cc93bf84b59f59
mayuresh-halshikar <mayureshhalshikar@willyweather.com>
2017-09-25 17:17:36 +0530

    https://github.com/projects-ebspl/invensist/issues/46

---
+++ src/main/java/com/einsicht/controller/ForwardController.java
@@ -0,0 +1,19 @@
+package com.einsicht.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ForwardController {
+	
+	@RequestMapping(value="/page/{page}", method = RequestMethod.GET)
+	public ModelAndView forward(@PathVariable("page") String page) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName(page);
+		return modelAndView;
+	}
+	
+}
