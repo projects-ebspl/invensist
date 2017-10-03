94bad55ec93f5ed76b8ca351f3107ca6fb9a9530
yogilalsinghmca <30467974+yogilalsinghmca@users.noreply.github.com>
2017-10-03 17:24:07 +0530

    cleanup task

--- src/main/java/com/einsicht/controller/LoginController.java
+++ src/main/java/com/einsicht/controller/LoginController.java
@@ -1,14 +1,9 @@
 package com.einsicht.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.einsicht.entities.User;
 
 
 @Controller
@@ -20,39 +15,7 @@ public class LoginController {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("login");
 		return modelAndView;
-	}
-	
-	
-	@RequestMapping(value="/registration", method = RequestMethod.GET)
-	public ModelAndView registration(){
-		ModelAndView modelAndView = new ModelAndView();
-		User user = new User();
-		modelAndView.addObject("user", user);
-		modelAndView.setViewName("registration");
-		return modelAndView;
-	}
-	
-	@RequestMapping(value = "/registration", method = RequestMethod.POST)
-	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
-		ModelAndView modelAndView = new ModelAndView();
-//		User userExists = userService.findUserByEmail(user.getEmail());
-//		if (userExists != null) {
-//			bindingResult
-//					.rejectValue("email", "error.user",
-//							"There is already a user registered with the email provided");
-//		}
-//		if (bindingResult.hasErrors()) {
-//			modelAndView.setViewName("registration");
-//		} else {
-//			userService.saveUser(user);
-//			modelAndView.addObject("successMessage", "User has been registered successfully");
-//			modelAndView.addObject("user", new User());
-//			modelAndView.setViewName("registration");
-//			
-//		}
-		return modelAndView;
-	}
-	
+	}	
 	@RequestMapping(value="/home", method = RequestMethod.GET)
 	public ModelAndView home(){
 		ModelAndView modelAndView = new ModelAndView();
